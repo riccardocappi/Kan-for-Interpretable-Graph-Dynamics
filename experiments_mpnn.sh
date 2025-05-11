bc2909e330569a30dad5647b527e95d193eb037c
Riccardo Cappi <riccardoblevio@gmail.com>
2025-05-11 20:56:46 +0000

    modified mpnn experiments file

--- experiments_mpnn.sh
+++ experiments_mpnn.sh
@@ -1,9 +1,8 @@
 conda activate myenv
-nohup  python main.py --config=./configs/config_kuramoto_mpnn.yml --method=optuna --n_trials=10 --study_name=kuramoto-mpnn_ --process_id=0 &
+nohup python main.py --config=./configs/config_neuronal_mpnn.yml --method=optuna --n_trials=15 --study_name=neuronal_mpnn_ --process_id=0 &
 sleep 1m
-python main.py --config=./configs/config_kuramoto_mpnn.yml --method=optuna --n_trials=10 --study_name=kuramoto-mpnn_ --process_id=1 &
+python main.py --config=./configs/config_neuronal_mpnn.yml --method=optuna --n_trials=15 --study_name=neuronal_mpnn_ --process_id=1 &
 sleep 1m
-python main.py --config=./configs/config_kuramoto_mpnn.yml --method=optuna --n_trials=10 --study_name=kuramoto-mpnn_ --process_id=2 &
+python main.py --config=./configs/config_neuronal_mpnn.yml --method=optuna --n_trials=15 --study_name=neuronal_mpnn_ --process_id=2 &
 wait
-
 echo "job done" > ./outputs/gkan-ode-mpnn.out &
